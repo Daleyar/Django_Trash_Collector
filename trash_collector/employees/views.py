6e8f7e094f183c8f8e51b72867f99ee3260d0123
Ali Daley <ali.daley72@gmail.com>
2021-09-01 10:29:24 -0400

    Created the employee model and added a create method with pathing to the employee side to finish registration.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.apps import apps
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
+from .models import Employee
 
 # Create your views here.
 
@@ -11,3 +14,20 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        user = request.user
+        email = request.POST.get('email')
+        city = request.POST.get('city')
+        street_address = request.POST.get('street_address')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip_code')
+        new_employee = Employee(name=name, user=user, email=email, street_address=street_address, 
+        city=city, state=state, zip_code=zip_code
+        )
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
